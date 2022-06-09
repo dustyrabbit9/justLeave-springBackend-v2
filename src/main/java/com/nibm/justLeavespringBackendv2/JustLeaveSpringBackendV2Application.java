79360018780a33b3b358259f34ab941df70b2267
Imran Rasheed <imran.2001r@gmail.com>
2022-06-09 22:16:04 +0530

    Initial Commit

---
+++ src/main/java/com/nibm/justLeavespringBackendv2/JustLeaveSpringBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.nibm.justLeavespringBackendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustLeaveSpringBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustLeaveSpringBackendV2Application.class, args);
+	}
+
+}
