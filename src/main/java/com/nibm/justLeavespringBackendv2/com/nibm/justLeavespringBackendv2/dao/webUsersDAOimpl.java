9a8f8d961ef6dad97b1ff1f647f3674abca79618
navintc <navin.thamindu@gmail.com>
2022-06-11 12:17:01 +0530

    Adding get all for webuser data

--- src/main/java/com/nibm/justLeavespringBackendv2/com/nibm/justLeavespringBackendv2/dao/webUsersDAOimpl.java
+++ src/main/java/com/nibm/justLeavespringBackendv2/com/nibm/justLeavespringBackendv2/dao/webUsersDAOimpl.java
@@ -1,12 +1,18 @@
 package com.nibm.justLeavespringBackendv2.com.nibm.justLeavespringBackendv2.dao;
 
 import com.nibm.justLeavespringBackendv2.model.webUsers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class webUsersDAOimpl implements  webUsersDAO{
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public int save(webUsers webuser) {
@@ -25,7 +31,7 @@ public class webUsersDAOimpl implements  webUsersDAO{
 
     @Override
     public List<webUsers> getAll() {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM web_users;", new BeanPropertyRowMapper<webUsers>(webUsers.class));
     }
 
     @Override
